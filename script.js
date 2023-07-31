a0a73890b0daf1acee084b2beb44407e551772a9
Tushar ka <tushar.karan@tcs.com>
2023-08-01 00:06:19 +0530

    first push page

--- script.js
+++ script.js
@@ -16,3 +16,11 @@ responsive:{
 }
 })
 });
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "280px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
